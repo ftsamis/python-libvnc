134d4d3e2ff698772635cfe729aee383cb16adf3
Fotis Tsamis <ftsamis@gmail.com>
2017-03-05 19:19:07 +0200

    Create a libvncclient python package for better file structure.

---
+++ client/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+import os
+import re
+import subprocess
+import distutils.command.build_ext
+from distutils.version import LooseVersion
+from distutils.spawn import find_executable
+from distutils.core import setup, Extension
+
+SWIG_MIN_VERSION    = "3.0" 
+
+class Build_Ext_find_swig3(distutils.command.build_ext.build_ext):
+    """ Doc:
+    https://pythonhosted.org/bob.extension/_modules/distutils/command/build_ext.html
+    https://github.com/vishnubob/wub/blob/master/setup.py
+    """
+
+    def find_swig(self):
+
+        if os.name != "posix":
+            # Call parent function
+            return super(Build_Ext_find_swig3, self).find_swig()
+        else:
+            return self.get_swig_executable()
+
+    def get_swig_executable(self):
+        # Source https://github.com/FEniCS/ffc/blob/master/setup.py
+        "Get SWIG executable"
+
+        # Find SWIG executable
+        swig_executable = None
+        for executable in ["swig3.0", "swig"]:
+            swig_executable = find_executable(executable)
+            if swig_executable is not None:
+                # Check that SWIG version is ok
+                output = subprocess.check_output(
+                            [swig_executable, "-version"]).decode('utf-8')
+                swig_version = re.findall(r"SWIG Version ([0-9.]+)", output)[0]
+                if LooseVersion(swig_version) >= LooseVersion(SWIG_MIN_VERSION):
+                    break
+                swig_executable = None
+        if swig_executable is None:
+            raise OSError("Unable to find SWIG version %s or higher." % SWIG_MIN_VERSION)
+        print("Found SWIG: %s (version %s)" % (swig_executable, swig_version))
+        return swig_executable
+
+
+
+libvncclient_module = Extension('_libvncclient',
+                               sources=['libvncclient/libvncclient.i'],
+                               include_dirs=['/usr/local/include'],
+                               library_dirs=['/usr/local/lib'],
+                               libraries=['vncclient'],
+                               )
+
+keysym_module = Extension('_keysym',
+                           sources=['libvncclient/keysym.i'],
+                           swig_opts=['-I/usr/local/include'],
+                           include_dirs=['/usr/local/include'],
+                           library_dirs=['/usr/local/lib'],
+                           libraries=['vncclient'],
+                           )
+
+setup (name = 'libvncclient',
+       version = '0.1',
+       author      = "Fotis Tsamis",
+       description = """Python bindings for libvncclient""",
+       ext_package = "libvncclient",
+       ext_modules = [libvncclient_module, keysym_module],
+       packages = ["libvncclient"],
+       cmdclass={
+        "build_ext": Build_Ext_find_swig3
+       }
+       )
+
